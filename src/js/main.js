1b3fddb585ee77986641024a67b6e87091874c04
TimaStramcov <tstramcov@mail.ru>
2021-10-04 13:38:41 +0300

    work js

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+import './slider';
+import modals from './moduls/modal';
+import tabs from './moduls/tabs';
+import forms from './moduls/forms';
+import changeModalState from './moduls/changeModalState';
+import timer from './moduls/timer';
+import images from './moduls/images';
+
+window.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+
+    let modalState = {};
+    let deadline = '2021-10-11';
+
+    changeModalState(modalState);
+    modals();
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
+    forms(modalState);
+    timer('.container1', deadline);
+    images();
+});
